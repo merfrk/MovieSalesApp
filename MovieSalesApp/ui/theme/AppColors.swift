d3b4a91397c6f39e556c0e8e89a8601c65440a6a
merfrk <o.farukartan@hotmail.com>
2025-09-19 13:43:09 +0300

    feat: add AppColors and font

---
+++ MovieSalesApp/ui/theme/AppColors.swift
@@ -0,0 +1,18 @@
+//
+//  AppColors.swift
+//  MovieSalesApp
+//
+//  Created by Omer on 19.09.2025.
+//
+
+import Foundation
+import SwiftUI
+
+struct AppColors {
+    static let primary = Color("Primary")
+    static let secondary = Color("Secondary")
+    static let background = Color("BackgroundColor")
+    static let text = Color("TextColor")
+    static let beige = Color("Beige")
+    static let peach = Color("Peach")
+}
