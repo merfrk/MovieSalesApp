8e544970fdf0d3e2de976912a49ad75d33ba52e9
merfrk <o.farukartan@hotmail.com>
2025-09-19 12:17:53 +0300

    initial commit

---
+++ MovieSalesApp/MovieSalesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSalesAppApp.swift
+//  MovieSalesApp
+//
+//  Created by Omer on 19.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieSalesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
